47422aa00cddd3efb747ff114ebdd51e9f9c6f53
Vahan <vahansargsyan1310@gmail.com>
2020-08-08 14:55:55 +0400

    add html file

---
+++ script.js
@@ -0,0 +1,11 @@
+var colors = [
+  "rgb(75,187,209)",
+  "rgb(2,164,106)",
+  "rgb(237,114,37)",
+  "rgb(61,48,145)",
+];
+
+//word, distance_betvene_circels, size_of_circle, colors, fontSize, font
+var banner = new Banner("Hppy_B", 5, 2, colors, 100, "Arial");
+//canvas_id, width, height
+banner.initialize("canvas", 960, 130);
